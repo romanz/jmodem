57d0d5224427f789fa29707113814e8bd9250d3b
Roman Zeyde <roman.zeyde@gmail.com>
2015-02-15 14:02:15 +0200

    Sampler: add first version

---
+++ src/jmodem/Sampler.java
@@ -0,0 +1,64 @@
+package jmodem;
+
+import java.io.IOException;
+
+public class Sampler implements Recorder {
+	
+	public final int width = 128;
+	public final int resolution = 1024;
+	
+	private final int N = width * resolution;	
+	private final float[][] filt;
+	private final int coeffs_len;
+	private float time;
+	private float freq;
+	private float[] buff;
+	private int index;
+	private Recorder src;
+	
+	public Sampler(Recorder source) {
+		src = source;
+		float[] h = new float[2*N];
+		for (int i = -N; i < N; i++) {
+			double u = i;
+			double window = Math.cos(0.5 * Math.PI * u / N);
+			h[N+i] = (float)(sinc(Math.PI * u / resolution) * window);			
+		}
+		coeffs_len = 2 * width;
+		filt = new float[resolution][coeffs_len];
+		for (int i = 0; i < resolution; i++) {
+			for (int j = 0; j < coeffs_len; j++) {
+				filt[i][j] = h[i + (coeffs_len - 1 - j) * resolution];
+			}
+		}
+		
+		buff = new float[coeffs_len];  // zeroes (by default)
+		index = 0;
+		time = width + 1;
+		freq = 1f;
+	}
+
+	private double sinc(double d) {
+		return d != 0 ? Math.sin(d) / d : 1.0;
+	}
+
+	@Override
+	public void read(float[] buffer, int offset, int size) throws IOException {
+		for (int o = offset; o < size; o++) {
+			int k = (int)time;
+			int j = ((int)(time - k)) * resolution;
+			float[] coeffs = filt[j];		
+			for (int end = k + width; index < end; index++) {
+				System.arraycopy(buff, 1, buff, 0, buff.length - 1);
+				src.read(buff, buff.length - 1, 1);
+			}
+			time += freq;
+			float result = 0f;
+			for (int i = 0; i < buff.length; i++) {
+				result += (coeffs[i] * buff[i]);				
+			}
+			buffer[o] = result;			
+		}
+	}
+
+}
---
+++ test/jmodem/SamplerTest.java
@@ -0,0 +1,52 @@
+package jmodem;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class SamplerTest {
+	
+	class RecorderTest implements Recorder {
+
+		public float[] buffer;
+		int offset;
+
+		RecorderTest(int size) {
+			buffer = new float[size];
+			offset = 0;
+		}
+		
+		@Override
+		public void read(float[] buffer, int offset, int size) throws IOException {
+			if (this.offset + size > this.buffer.length) {
+				throw new IOException("EOF");
+			}
+			System.arraycopy(this.buffer, this.offset, buffer, offset, size);
+			this.offset += size;
+		}
+		
+	}
+
+	@Test
+	public void test() throws IOException {		
+		RecorderTest r = new RecorderTest(2048);
+		for (int i = 0; i < r.buffer.length; i++) {
+			r.buffer[i] = i;
+		}
+		Sampler s = new Sampler(r);
+		float[] b = new float[1024];
+		s.read(b, 0, b.length);		
+		for (int i = 0; i < b.length; i++) {
+			assertEquals(b[i], i + s.width, 1e-15);
+		}
+		try {
+			s.read(b, 0, b.length);
+			fail("should raise");
+		} catch (IOException e) {
+			assert e.getMessage() == "EOF";
+		}
+	}
+
+}
