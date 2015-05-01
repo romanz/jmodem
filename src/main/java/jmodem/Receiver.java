ef8e8d03e0af5642854a415b27c5723c6f016f19
Roman Zeyde <roman.zeyde@gmail.com>
2015-05-01 09:37:05 +0300

    Receiver: add logging

--- src/main/java/jmodem/Receiver.java
+++ src/main/java/jmodem/Receiver.java
@@ -6,21 +6,30 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Receiver {
+	
+	private static final Logger log = Logger.getLogger("Receiver");
 
 	public static void run(InputSampleStream src, OutputStream dst) throws IOException {
+		log.info("detecting");
 		Detector d = new Detector(src);
 		d.run();
-		double freq = 1.0 / (1 + d.frequencyDrift());
-		src = new Sampler(src, freq);
-		// TODO: verify prefix after fixing frequency drift
+		double drift = d.frequencyDrift();
+		log.info("drift: " + drift * 1e6 + " ppm");
+		src = new Sampler(src, 1.0 / (1 + drift));		
 
+		log.info("training");
 		Equalizer eq = new Equalizer(9, 8);
 		Filter filt = eq.run(src);
 
+		log.info("demodulating");
 		Demodulator r = new Demodulator(src, filt);
 		r.run(dst);
+
+		log.info("done");
 	}
 
 	static class InputStreamWrapper implements InputSampleStream {
@@ -44,6 +53,9 @@ public class Receiver {
 	}
 
 	public static void main(String[] args) throws Exception {
+		Logger root = Logger.getGlobal();
+		root.setLevel(Level.ALL);
+		
 		InputSampleStream src = new InputStreamWrapper(System.in);
 		run(src, System.out);
 	}
